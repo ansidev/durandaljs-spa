a76e375a73266153d38035a2af94d65c4fef85cf
Le Minh Tri <ansidev@gmail.com>
2015-02-23 18:26:04 +0700

    Lint file

--- app/viewmodels/movies/index.js
+++ app/viewmodels/movies/index.js
@@ -1,12 +1,18 @@
 define(function (require) {
-    var ko = require('knockout');
-    var moviesData = require("data/moviesData");
+    var ko = require('knockout'),
+//        router = require('plugins/router'),
+        moviesData = require("data/moviesData");
     
     return {
+//        router: router,
         movies: ko.observable(),
         
         activate: function() {
             this.movies(moviesData.indexMovies());
+//            router.map([
+//                { route: 'movies/add', title: 'Add movie', moduleId: 'viewmodels/movies/add', nav: true },
+//            ]).buildNavigationModel().activate();
+
         }
-    }
+    };
 });
